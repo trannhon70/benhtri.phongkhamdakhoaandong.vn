16481ff97cd29334cd147d3985e92e8809f57576
trannhon70 <kevintran351996@gmail.com>
2024-11-05 10:13:45 +0700

    update

--- inc/header.php
+++ inc/header.php
@@ -38,7 +38,13 @@ $local ='http://localhost/benhtri.phongkhamdakhoaandong.vn'
     <title>Phòng khám đa khoa</title>
     <link rel="preload" href="<?php echo $local ?>/ckfinder/userfiles/images/Chat/Chat-Dakhoa.gif" as="image">
 
-    
+    <!-- Google Tag Manager -->
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-5NN5ZWN2');</script>
+    <!-- End Google Tag Manager -->
     <style amp-boilerplate>
         body {
             -webkit-animation: -amp-start 8s steps(1, end) 0s 1 normal both;
@@ -180,3 +186,4 @@ $local ='http://localhost/benhtri.phongkhamdakhoaandong.vn'
             });
         });
     </script>
+
